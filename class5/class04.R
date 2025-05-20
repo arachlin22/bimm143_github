83251b7346fbbe0a23cab594a5bd1e22029888fa
Abraham Rachlin <arachlin@ucsd.edu>
2025-05-20 11:51:21 -0700

    add class5

---
+++ class5/class04.R
@@ -0,0 +1,11 @@
+#' ---
+#' title: "Intro to R"
+#' author: "Abraham Rachlin, A16735986"
+#' date: "April 10, 2025"
+#' ---
+
+# My first R script
+x <- 1:50
+plot(x, sin(x))
+plot(x, sin(x), typ="l", col="green", lwd=3, xlab="Silly x axis", ylab="Sensible y axis")
+x <- 1:10
